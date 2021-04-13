fffa37aae5dda9c4700d1eab0f7a6fc5d26bcebe
Tomasz Nowak <tomasz.nowak@mycit.ie>
2021-04-13 22:37:06 +0100

    I have created variable choice_main and infinite loop for the MAIN_MENU. Added validation and the comment in case of ValueError occurs.

--- main.py
+++ main.py
@@ -10,6 +10,30 @@ MAIN_MENU = "Running Contest \n=========================== \n1. Show the results
 
 def main():
     print(MAIN_MENU)
+    try:
+        choice_main = int(input("==>"))
+        while True:
+            if choice_main == 1:
+                print("(1) Show the results for a race \n===============================")
+            elif choice_main == 2:
+                print("(2) Add results for a race \n===============================")
+            elif choice_main == 3:
+                print("(3) Show all competitors by county \n===============================")
+            elif choice_main == 4:
+                print("(4) Show the winner of each race \n===============================")
+            elif choice_main == 5:
+                print("(5) Show all the race times for one competitor \n===============================")
+            elif choice_main == 6:
+                print("(6) Show all competitors who have won a race \n===============================")
+            elif choice_main == 7:
+                print("Thank you, Goodbye.")
+                break
+            else:
+                print("Choose one of the options from 1 to 7.")
+            print(MAIN_MENU)
+            choice_main = int(input("==>"))
+    except ValueError:
+        print("Please choose one of the options from main menu.")
 
 
 if __name__ == '__main__':
