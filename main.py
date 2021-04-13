b442579be3f96268e8985b423f35546d90a80780
Tomasz Nowak <tomasz.nowak@mycit.ie>
2021-04-13 22:13:05 +0100

    Today I started the project. I have created file main.py. Added main() function and comments and main() function call. Added constant MAIN_MENU.

---
+++ main.py
@@ -0,0 +1,16 @@
+# Tomasz Nowak
+# 13 APR 2021
+
+# Creating main menu as a constant
+MAIN_MENU = "Running Contest \n=========================== \n1. Show the results for a race" \
+            + "\n2. Add results for a race \n3. Show all competitors by county \n4. Show the winner of each race" \
+            + "\n5. Show all the race times for one competitor \n6. Show all competitors who have won a race" \
+            + "\n7. Quit"
+
+
+def main():
+    print(MAIN_MENU)
+
+
+if __name__ == '__main__':
+    main()
